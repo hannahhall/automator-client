176fe74872649b3f8d15c475f6b242c76ab9807a
hannahhall <hannahkhall13@gmail.com>
2022-09-14 16:12:34 -0500

    login test cont

--- __tests__/pages/login.test.jsx
+++ __tests__/pages/login.test.jsx
@@ -1,10 +1,18 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import Login from '../../pages/login';
 import { AuthProvider } from '../../hooks/useAuth';
 import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+import mockAxios from 'jest-mock-axios';
 
 jest.mock('next/router', () => require('next-router-mock'));
+const pushMock = jest.fn()
+const router = jest.spyOn(require('next/router'), 'useRouter').mockReturnValue({ push: pushMock });
+
 describe('Login', () => {
+  afterEach(() => {
+    mockAxios.reset();
+  });
   it('renders a heading', () => {
     render(
       <AuthProvider>
@@ -17,5 +25,39 @@ describe('Login', () => {
     });
 
     expect(heading).toBeInTheDocument();
+  });
+
+  it('should reroute on successful login', async () => {
+    const user = userEvent.setup();
+
+    const username = 'username';
+    const password = 'password';
+
+    render(
+      <AuthProvider>
+        <Login />
+      </AuthProvider>
+    );
+
+
+    mockAxios.get.mockResolvedValueOnce({
+      data: {
+        access: 'token1',
+        refresh: 'token2'
+      }
+    });
+    const usernameInput = screen.getByLabelText('Username');
+    await user.type(usernameInput, username);
+
+    const passwordInput = screen.getByLabelText('Password');
+    await user.type(passwordInput, password);
+
+    await user.click(screen.getByText('Submit'));
+
+    await waitFor(() => {
+      expect(pushMock.mock.calls.length).toBe(1);
+    });
+
+
   });
 });
